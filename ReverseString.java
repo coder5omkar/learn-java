a5cbfafe647d171c7e80842867707892c7702534
coder5omkar <coderomkar5@gmail.com>
2025-12-04 12:05:45 +0530

    ctn

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        // logic 1 + via String concatenation
+        String str = "kamda";
+        StringBuilder rev = new StringBuilder();
+
+/*        int L = str.length();
+
+        System.out.println("Length of String :"+L);
+
+        while(L>0){
+            rev.append(str.charAt(L - 1));
+            L--;
+        }*/
+
+        System.out.println(rev);
+
+
+
+
+    }
+}
